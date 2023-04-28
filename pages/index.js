1aeaafd98ac7e0416d05bfcef6376e890cee9aa0
Louis Weiser <louis.weiser98@gmail.com>
2023-04-28 22:54:11 +0200

    testing api and is working

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import { DataContext } from "@/contexts/dataContext.js";
+import { useContext } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data } = useContext(DataContext);
+  console.log(data);
   return (
     <>
       <Head>
@@ -14,6 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main></main>
     </>
   );
